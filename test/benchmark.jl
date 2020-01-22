b3d391211bfa5e2c1a9be65559f3c943379e3aaa
Johannes Boehm <johannes.boehm@gmail.com>
2020-01-22 22:15:17 +0100

    update_weights

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -1,6 +1,10 @@
 
 #include("../src/GLFixedEffectModels.jl")
 
+# Benchmark on Intel i7-6700 with 4 cores @ 3.40 GHz, 64GB RAM
+# running Windows 10 Enterprise, Julia 1.3.1, CUDA 10.2, R 3.6.2
+# fixest 0.2.1
+
 using DataFrames, GLM, Random
 using GLFixedEffectModels
 using BenchmarkTools
@@ -9,7 +13,7 @@ BenchmarkTools.DEFAULT_PARAMETERS.samples = 10
 BenchmarkTools.DEFAULT_PARAMETERS.seconds = 60
 
 rng = MersenneTwister(1234)
-N = 1_000_000
+N = 2_000_000
 K = 100
 id1 = rand(rng, 1:(N/K), N)
 id2 = rand(rng, 1:K, N)
@@ -20,10 +24,19 @@ df = DataFrame(id1 = categorical(string.(id1)), id1_noncat = id1, id2 = categori
 
 
 
+m = @formula y ~ x1 + x2 + GLFixedEffectModels.fe(id1) + GLFixedEffectModels.fe(id2)
+@profile GLFixedEffectModels.nlreg(df, m, Poisson(), GLM.LogLink() , start = [0.2;0.2] )
+Profile.print()
+Profile.clear()
+
+@time GLFixedEffectModels.nlreg(df, m, Poisson(), GLM.LogLink() , start = [0.2;0.2] )
+
+@profile (for i = 1:5; GLFixedEffectModels.nlreg(df, m, Poisson(), GLM.LogLink() , start = [0.2;0.2] ); end)
+
 # Benchmark GLFixedEffectModels
 
-# One FE
-m = @formula y ~ x1 + x2 + fe(id1)
+# Two FE
+m = @formula y ~ x1 + x2 + fe(id1) + fe(id2)
 @benchmark x = nlreg(df, m, Poisson(), GLM.LogLink() , start = [0.2;0.2] )
 # BenchmarkTools.Trial:
 #   memory estimate:  1.29 GiB
@@ -66,21 +79,37 @@ end
 #   evals/sample:     1
 
 
-# Benchmark fixest
+# BENCHMARK FIXEST
+
+# One thread
 function runme()
     R"res <- fixest::feglm(y ~ x1 + x2  | id1, df_r, family = \"poisson\", nthreads = 1, start = c(0.2,0.2))"
 end
-@benchmark runme()
+function runme()
+    R"res <- fixest::feglm(y ~ x1 + x2  | id1 + id2, df_r, family = \"poisson\", nthreads = 1, start = c(0.2,0.2))"
+end
+# Two FE's
 # BenchmarkTools.Trial:
 #   memory estimate:  1.09 KiB
 #   allocs estimate:  32
 #   --------------
-#   minimum time:     1.839 s (0.00% GC)
-#   median time:      3.827 s (0.00% GC)
-#   mean time:        3.607 s (0.00% GC)
-#   maximum time:     7.438 s (0.00% GC)
+#   minimum time:     3.201 s (0.00% GC)
+#   median time:      3.393 s (0.00% GC)
+#   mean time:        3.392 s (0.00% GC)
+#   maximum time:     3.725 s (0.00% GC)
 #   --------------
 #   samples:          10
 #   evals/sample:     1
-# clear everything in R
+@benchmark runme()
+
+# As many threads as you like
+function runme()
+    R"res <- fixest::feglm(y ~ x1 + x2  | id1, df_r, family = \"poisson\", start = c(0.2,0.2))"
+end
+function runme()
+    R"res <- fixest::feglm(y ~ x1 + x2  | id1 + id2, df_r, family = \"poisson\", start = c(0.2,0.2))"
+end
+@benchmark runme()
+
+
 R"rm(list = ls())"
