475d730c7c0fbb74ab192857a8898dfe2785486a
caibengbu <sunye@uchicago.edu>
2021-07-08 13:00:45 -0500

    change from using RCall to using hardcoded value, remove RCall from Project.toml

--- test/biascorr_test.jl
+++ test/biascorr_test.jl
@@ -5,6 +5,7 @@ using RDatasets, Test, Random
 using StableRNGs
 
 using GLM: LogitLink, ProbitLink
+# using RCall
 
 rng = StableRNG(1234)
 
@@ -17,35 +18,32 @@ a = ["A","B","C"]
 df.Random = vec([a[i] for i in idx])
 df.RandomCategorical = df.Random
 
-# Set up R
-using RCall
-R"""
-install.packages("alpaca")
-"""
-R"library(alpaca)"
-df_r = deepcopy(df)
-@rput df_r
-
 # Test 1: Two-way Logit
 # See if the coefficients after bias correction match with the results obtained from R package alpaca
-R"""
+
+#= R"""
 res1 <- alpaca::feglm(binary ~ SepalWidth | SpeciesDummy + RandomCategorical , df_r, binomial(), beta.start = c(0.2))
 res_bc1 <- alpaca::biasCorr(res1)
 coef1 <- res_bc1[["coefficients"]]
 """
 @rget coef1
+###############################
+coef1 = 7.214197357443702
+###############################
+=#
 
 m = GLFixedEffectModels.@formula binary ~ SepalWidth + GLFixedEffectModels.fe(SpeciesDummy) + GLFixedEffectModels.fe(RandomCategorical)
 x = GLFixedEffectModels.nlreg(df, m, Binomial(), LogitLink(), start = [0.2], save=true)
 x_afterbc = GLFixedEffectModels.BiasCorr(x, df)
 
-@test x_afterbc.coef ≈ [coef1] atol = 1e-4
+@test x_afterbc.coef ≈ [7.214197357443702] atol = 1e-4
 
 # Test 2: Two-way Probit
 # Since the coefficients for models that use the probit link didn't match the results from R's alpaca before bias correction
 # We test if the ``net adjusted value`` for the coefficients matches, for now.
 # TODO: check why probit link didn't work as expected
-R"""
+
+#=R"""
 res2 <- alpaca::feglm(binary ~ SepalWidth | SpeciesDummy + RandomCategorical , df_r, binomial(link = "probit"), beta.start = c(0.2))
 res_bc2 <- alpaca::biasCorr(res2)
 coef2<- res2[["coefficients"]]
@@ -53,32 +51,43 @@ coef2_afterbc <- res_bc2[["coefficients"]]
 net_adjusted_value = coef2_afterbc - coef2
 """
 @rget net_adjusted_value
+###############################
+net_adjusted_value = -0.5381054072387927
+###############################
+=#
 
 x = GLFixedEffectModels.nlreg(df, m, Binomial(), ProbitLink(), start = [0.2], save=true)
 x_afterbc = GLFixedEffectModels.BiasCorr(x, df)
-@test x_afterbc.coef - x.coef ≈ [net_adjusted_value] atol = 1e-4
+@test x_afterbc.coef - x.coef ≈ [-0.5381054072387927] atol = 1e-4
 
 # Test 3: Three-way Logit (I = 5, J = 6, T = 7), Network structure
+I, J, T = 5, 6, 7
+i_index = repeat(1:I,inner = J*T)
+j_index = repeat(1:J,outer = I, inner = T)
+t_index = repeat(1:T,outer = I*J)
+
+# Reset rng
+rng = StableRNG(1234)
+data = DataFrame(i = i_index, j = j_index, t = t_index, x = rand(rng, I*J*T), y = rand(rng,Bernoulli(), I*J*T))
+
+#= @rput data
 R"""
-data <- alpaca::simGLM(n = 30, t = 7, seed = 1234, model = "logit")
-colnames(data)[1] <- "ij"
-ij <- merge(seq(5),seq(6))
-data$i <- ij$x
-data$j <- ij$y
-data_sorted <- data[order(data$i, data$j, data$t),]
-res3 <- alpaca::feglm(y ~ x1 | i + j + t , data_sorted, binomial(), beta.start = c(0.2))
-res3.bc_L0 <- biasCorr(res3,panel.structure = 'network',L = 0)
-res3.bc_L3 <- biasCorr(res3,panel.structure = 'network',L = 3)
-coef3 <- res3[["coefficients"]]
+res3 <- alpaca::feglm(y ~ x | i + j + t , data, binomial(), beta.start = c(0.2))
+res3.bc_L0 <- alpaca::biasCorr(res3,panel.structure = 'network',L = 0)
+res3.bc_L3 <- alpaca::biasCorr(res3,panel.structure = 'network',L = 3)
 coef_L0 <- res3.bc_L0[["coefficients"]]
 coef_L3 <- res3.bc_L3[["coefficients"]]
 """
-@rget data_sorted coef3 coef_L0 coef_L3
+@rget coef_L0 coef_L3 
+###############################
+coef_L0 = -0.8958868087842135
+coef_L3 = -0.8845864617809618
+###############################
+=#
 
-m = GLFixedEffectModels.@formula y ~ x1 + GLFixedEffectModels.fe(i) + GLFixedEffectModels.fe(j) + GLFixedEffectModels.fe(t)
-x = GLFixedEffectModels.nlreg(data_sorted, m, Binomial(), LogitLink(), start = [0.2], save=true)
-x_bc_L0 = GLFixedEffectModels.BiasCorr(x, data_sorted; panel_structure = "network", L = 0)
-x_bc_L3 = GLFixedEffectModels.BiasCorr(x, data_sorted; panel_structure = "network", L = 3)
-
-@test x_bc_L0.coef ≈ [coef_L0] atol = 1e-4
-@test x_bc_L3.coef ≈ [coef_L3] atol = 1e-4
+m = GLFixedEffectModels.@formula y ~ x + GLFixedEffectModels.fe(i) + GLFixedEffectModels.fe(j) + GLFixedEffectModels.fe(t)
+x = GLFixedEffectModels.nlreg(data, m, Binomial(), LogitLink(), start = [0.2], save=true)
+x_bc_L0 = GLFixedEffectModels.BiasCorr(x, data; panel_structure = "network", L = 0)
+x_bc_L3 = GLFixedEffectModels.BiasCorr(x, data; panel_structure = "network", L = 3)
+@test x_bc_L0.coef ≈ [-0.8958868087842135] atol = 1e-4
+@test x_bc_L3.coef ≈ [-0.8845864617809618] atol = 1e-4
